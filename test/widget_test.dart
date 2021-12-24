f0adc831b235a5ba183f635329d95ad48054ab7b
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2021-12-24 21:18:36 +0500

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:vehicle_management_app/main.dart';
+import 'package:lit_firebase_ui_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
