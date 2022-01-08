734b368260151550048c26d2ea3010cf1cfb8187
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-01-08 23:17:52 +0500

    Create homepage.dart

---
+++ lib/Screens/homepage.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class main_page extends StatefulWidget {
+  @override
+  State<main_page> createState() => _main_pageState();
+}
+
+class _main_pageState extends State<main_page> {
+  int _currentIndex=0;
+  final tabs=[
+    home(),
+    Center(child: Text("Search"),),
+    Center(child: Text("Profile"),)
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+            title: const Text('Home_Page'),
+            backgroundColor: Color(0xFF37474F),
+            leading: Image.asset("images/splash.png")
+        ),
+        body: tabs[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          fixedColor: Colors.amber,
+          backgroundColor: Color(0xFF37474F),
+          iconSize: 30,
+          selectedFontSize: 17,
+          currentIndex: _currentIndex,
+          items:[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Search',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Profile',
+            ),
+          ],
+          onTap: (index){
+            setState(() {
+              _currentIndex=index;
+            });
+          },
+        ));
+  }
+}
+
