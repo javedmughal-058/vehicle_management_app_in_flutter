9b904b909d8693d121698dbd25cdd6ffa99bcbaf
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-01-09 01:40:25 +0500

    Update car_detail.dart

--- lib/Screens/car_detail.dart
+++ lib/Screens/car_detail.dart
@@ -18,11 +18,11 @@ class _car_detailState extends State<car_detail> {
       body: ListView(
         children: [
           SizedBox(height: 15,),
-          Text("Shops_detail",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),),
+          Padding(padding:EdgeInsets.fromLTRB(10, 0, 0, 0),child: const Text("Shops_detail",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),)),
           SizedBox(height: 15,),
           Container(
             margin: EdgeInsets.only(left: 5,right: 5),
-            height: 90,
+            height: 80,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20),
                 color: Colors.white,
@@ -51,15 +51,59 @@ class _car_detailState extends State<car_detail> {
                     Text("10KM Away",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold),),
                   ],
                 ),
-                SizedBox(width: 90,),
+                Spacer(),
                 IconButton(
-                  icon: const Icon(Icons.remove_red_eye,size: 40,),
+                  tooltip: "Show full details",
+                  icon: const Icon(Icons.remove_red_eye,size: 30,),
                   color: Colors.amber,
                   onPressed: () {},
                 ),
               ],
             ),
           ),
+          SizedBox(height: 5,),
+          Container(
+            margin: EdgeInsets.only(left: 5,right: 5),
+            height: 80,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 7,
+                    blurRadius: 9,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ]
+            ),
+            child: Row(
+              children: [
+                SizedBox(width: 20,),
+                CircleAvatar(
+                  backgroundColor: Colors.grey,
+                  radius: 30,
+                ),
+                SizedBox(width: 20,),
+                Column(
+                  children: [
+                    SizedBox(height: 20,),
+                    Text("Shope Name",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
+                    SizedBox(height: 10,),
+                    Text("10KM Away",style: TextStyle(color: Colors.green,fontWeight: FontWeight.bold),),
+                  ],
+                ),
+                Spacer(),
+                IconButton(
+                  tooltip: "Show full details",
+                  icon: const Icon(Icons.remove_red_eye,size: 30,),
+                  color: Colors.amber,
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+
         ],
       ),
     );
