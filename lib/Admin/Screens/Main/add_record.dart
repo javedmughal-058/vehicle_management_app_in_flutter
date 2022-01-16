00d80d5a8af9faf5fec4e6fb74b248cd2033e23a
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-01-16 23:48:11 +0500

    Create add_record.dart

---
+++ lib/Admin/Screens/Main/add_record.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+class add_record extends StatefulWidget {
+  const add_record({Key? key}) : super(key: key);
+
+  @override
+  _add_recordState createState() => _add_recordState();
+}
+
+class _add_recordState extends State<add_record> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
