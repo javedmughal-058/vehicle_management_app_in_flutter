397e39e480b23a3453c8d3cd258e00693cb1ec87
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-01-16 23:48:08 +0500

    Update admin_home.dart

--- lib/Admin/Screens/Main/admin_home.dart
+++ lib/Admin/Screens/Main/admin_home.dart
@@ -15,7 +15,7 @@ class _admin_homeState extends State<admin_home> {
     return ListView(
       physics: ClampingScrollPhysics(),
       shrinkWrap: true,
-      padding: const EdgeInsets.all(45),
+      padding: const EdgeInsets.all(30),
       children: [
         SingleChildScrollView(
             scrollDirection: Axis.horizontal,
@@ -260,6 +260,111 @@ class _admin_homeState extends State<admin_home> {
                                 children: [
                                   //SizedBox(width: 20,),
                                   Text("Bike Wash Shops",style: GoogleFonts.merriweather(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                  Spacer(),
+                                  Text("10",style: GoogleFonts.merriweather(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                ],
+                              ),
+                              SizedBox(height: 20,),
+                              Row(
+                                children: [
+                                  //SizedBox(width: 20,),
+                                  Text("Tyre Shops",style: GoogleFonts.merriweather(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                  Spacer(),
+                                  Text("14",style: GoogleFonts.merriweather(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                ],
+                              ),
+
+
+                            ],
+                          ),
+                        ),
+
+                      ],
+                    ) ,
+                  ),
+                  SizedBox(width: 10,),
+                  Container(
+                    // padding: EdgeInsets.all(10),
+                    height: 400,
+                    width: 320,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(40.0),
+                          topLeft: Radius.circular(40.0),
+                          bottomLeft: Radius.circular(40.0),
+                          bottomRight: Radius.circular(40.0)
+                      ),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Colors.blue,
+                          Colors.indigo,
+                        ],
+                      ),
+                    ),
+                    child:Column(
+                      children: [
+                        Image.asset("images/batry.png",height: 100,),
+                        Text("Battery/Wash/Tyre",style: GoogleFonts.merriweather(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold
+                        ),),
+                        SizedBox(height: 20,),
+                        Container(
+                          padding: EdgeInsets.all(15),
+                          height: 220,
+                          width: 250,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(30.0),
+                                topLeft: Radius.circular(30.0),
+                                bottomLeft: Radius.circular(30.0),
+                                bottomRight: Radius.circular(30.0)
+                            ),
+                            color: Colors.white,
+                          ),
+                          child: Column(
+                            children: [
+                              SizedBox(height: 10,),
+                              Row(
+                                children: [
+                                  //SizedBox(width: 20,),
+                                  Text("Battery Shops",style: GoogleFonts.merriweather(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                  Spacer(),
+                                  Text("20",style: GoogleFonts.merriweather(
+                                      fontSize: 14,
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.bold
+                                  ),),
+                                ],
+                              ),
+                              SizedBox(height: 20,),
+                              Row(
+                                children: [
+                                  //SizedBox(width: 20,),
+                                  Text("Wash Shops",style: GoogleFonts.merriweather(
                                       fontSize: 14,
                                       color: Colors.black,
                                       fontWeight: FontWeight.bold
