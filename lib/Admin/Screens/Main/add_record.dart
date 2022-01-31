8b33c6de78600faa5e60e2145a31b562f1c2a7b5
Muhammad Javed <91019922+javedmughal-058@users.noreply.github.com>
2022-01-31 09:57:27 +0500

    update design

--- lib/Admin/Screens/Main/add_record.dart
+++ lib/Admin/Screens/Main/add_record.dart
@@ -10,7 +10,27 @@ class _add_recordState extends State<add_record> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+        appBar: AppBar(title: Text("Add Record"), backgroundColor: Colors.indigo,),
+        body: Form(
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: Icon(Icons.person),
+                  hintText: 'Name of Owner7',
+                  labelText: 'Name *',
+                ),
+                onSaved: (String? value) {
+                  // This optional block of code can be used to run
+                  // code when the user saves the form.
+                },
+                validator: (String? value) {
+                  return (value != null && value.contains('@')) ? 'Do not use the @ char.' : null;
+                },
+              )
+            ],
+          ),
+        ),
     );
   }
 }
